f20efef89c36db7f4759334ef5748661c7021a76
cgil <lao.tseu.is.alive@gmail.com>
2025-07-18 16:40:11 +0200

    migrated common libs

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "geoTreeTable"
 	AppSnake   = "go-geo-tree-table"
-	VERSION    = "0.0.22"
+	VERSION    = "0.0.23"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-geo-tree-table"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
